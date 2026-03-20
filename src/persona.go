47d8159a8044de7ad2ba4465519d2ee48d54a251
aylen <aylenchiesaok@gmail.com>
2026-03-19 23:53:13 -0300

    persona funcs

---
+++ src/persona.go
@@ -0,0 +1,22 @@
+package main
+
+type Persona struct {
+	Nombre string
+	Edad   int
+}
+
+func (p Persona) Saludar() string {
+	return "Hola, soy " + p.Nombre
+}
+
+func NewPersona(Nombre string, Edad int) *Persona {
+	return &Persona{Nombre, Edad}
+}
+
+func (p Persona) GetNombre() string {
+	return p.Nombre
+}
+
+func (p Persona) GetEdad() int {
+	return p.Edad
+}
